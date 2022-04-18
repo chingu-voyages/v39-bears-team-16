49faf99a5595627bb893d07923204c08706f9573
Nguyen Trieu <danneytrieu@gmail.com>
2022-04-18 16:03:47 -0600

    added basic routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <div >
-     hello
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="sign-in" element={<SignIn />} />
+      <Route path="forgot-password" element={<ForgotPassword />} />
+    </Routes>
   );
 }
 
