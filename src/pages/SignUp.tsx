7f47cc6429365563d1ddb262974c62541bc39313
Nguyen Trieu <danneytrieu@gmail.com>
2022-04-18 19:20:03 -0600

    add signup form

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { FaUserAlt, FaTwitterSquare } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
 
-function Signup() {
-  return <div>Signup</div>;
+export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  console.log(errors);
+
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <FaTwitterSquare />
+      <h2>or use your email to register</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FaUserAlt />
+        <input
+          type="text"
+          placeholder="name"
+          {...register("name", { required: true, maxLength: 80 })}
+        />
+        <AiOutlineMail />
+        <input
+          type="email"
+          placeholder="email"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        <RiLockPasswordLine />
+        <input
+          type="password"
+          placeholder="password"
+          {...register("password", { required: true })}
+        />
+        <button type="submit">SIGN UP</button>
+      </form>
+      <h3>Already have an account?</h3>
+      <a href="/sign-in">Login Here</a>
+    </>
+  );
 }
-
-export default Signup;
