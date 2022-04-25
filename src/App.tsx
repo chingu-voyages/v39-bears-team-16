3a678d98c232cbe5f29be382b67152ab1090bb06
RDiMag <87457132+RDiMag@users.noreply.github.com>
2022-04-25 19:36:03 -0400

    with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import theme from './theme.js';
 import Home from './pages/Home';
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import ForgotPassword from "./pages/ForgotPassword";
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import ForgotPassword from './pages/ForgotPassword';
 
 const App = () => {
   // theme provider here
