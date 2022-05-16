9a719012928cee9b52a3fe69de08865fc5bc140b
jpatrici <janice.patricia@shopify.com>
2022-05-16 17:53:55 -0400

    Add env variable to run in different env

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_PROD, // change to REACT_APP_API_LOCAL if you want to run with backend locally
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
