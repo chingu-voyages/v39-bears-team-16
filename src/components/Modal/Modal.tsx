960350ba01687d03abb7e85ba22a10b29063b576
Marcellia <ankitaz.az@gmail.com>
2022-06-02 19:53:43 -0400

    added aria attributes

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -17,7 +17,7 @@ export interface ModalProps {
   TitleText: string;
 }
 
-export const Modal= ({
+export const Modal = ({
   isOpen,
   hide,
   modalContent,
@@ -25,11 +25,23 @@ export const Modal= ({
 }: ModalProps) => {
   const modal = (
     <div>
-      <Fade  />
-      <Container aria-modal aria-labelledby={TitleText} tabIndex={-1} role="dialog">
+      <Fade />
+      <Container
+        aria-modal
+        aria-labelledby={TitleText}
+        tabIndex={-1}
+        role="dialog"
+      >
         <TitleContainer>
           <Title>{TitleText}</Title>
-          <CloseButton type="button" data-dismiss="modal" aria-label="Close" onClick={hide}>X</CloseButton>
+          <CloseButton
+            type="button"
+            data-dismiss="modal"
+            aria-label="Close"
+            onClick={hide}
+          >
+            X
+          </CloseButton>
         </TitleContainer>
         <Content>{modalContent}</Content>
       </Container>
