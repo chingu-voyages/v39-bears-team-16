4fba1c4ea86981ae4475bfd48d4d94cbf5074a3b
jpatrici <janice.patricia@shopify.com>
2022-07-07 19:25:10 -0400

    fix card styling

--- src/components/DropDownMenuItem/DropDownMenuItem.styled.tsx
+++ src/components/DropDownMenuItem/DropDownMenuItem.styled.tsx
@@ -3,7 +3,6 @@ import { Button } from '../Button';
 
 export const StyledDropDownMenuItem = styled.div`
   position: relative;
-  width: 100%;
   background-color: ${({ theme }) => theme.color.white};
 `;
 
