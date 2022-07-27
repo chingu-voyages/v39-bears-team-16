da124052192b2d626d38229a6e7efcae82f4e4a0
Marcellia <ankitaz.az@gmail.com>
2022-07-27 11:52:48 -0400

    refactored editForm error handling

--- src/pages/Editor/Classes/UpdateClassModals/EditClassModal.tsx
+++ src/pages/Editor/Classes/UpdateClassModals/EditClassModal.tsx
@@ -8,8 +8,8 @@ import { Input, InputField, TextArea } from 'components/Input';
 import { ErrorMessageInterface } from 'types';
 import { EditClassModalProps } from 'pages/Editor/Classes/classTypes';
 import { CreateClassProps, editClass } from 'api/classes';
-
-import { FormErrorMessages, StyledErrorMessage } from 'components/ErrorMessage';
+import { v4 as uuidv4 } from 'uuid';
+import { StyledErrorMessage } from 'components/ErrorMessage';
 
 import { Modal } from 'components/Modal/Modal';
 import { Button, PrimaryButton } from 'components/Button';
@@ -32,7 +32,7 @@ const Classworks = styled.div`
   padding: 1em;
   overflow: hidden;
   box-shadow: 0 13px 27px -5px hsla(240, 30.1%, 28%, 0.25),
-    0 8px 16px -8px hsla(0, 0%, 0%, 0.3), 0 -6px 16px -6px hsla(0, 0%, 0%, 0.03);
+    0 8px 1px -8px hsla(0, 0%, 0%, 0.1), 0 -6px 16px -6px hsla(0, 0%, 0%, 0.03);
 `;
 
 /* eslint no-underscore-dangle: 0 */
@@ -46,7 +46,6 @@ export const EditClassModal = ({
   const {
     register,
     control,
-    setValue,
     handleSubmit,
     reset,
     formState: { errors },
@@ -68,8 +67,6 @@ export const EditClassModal = ({
   // ClassWork
 
   const handleCancelModal = () => {
-    setValue('name', item.name);
-    setValue('description', item.description);
     reset();
     toggle();
   };
@@ -78,8 +75,7 @@ export const EditClassModal = ({
     try {
       await editClass(data, item._id);
       fetchClasses();
-      reset();
-      toggle();
+      handleCancelModal();
     } catch (error) {
       setErrorMessages(error as ErrorMessageInterface[]);
     }
@@ -127,33 +123,51 @@ export const EditClassModal = ({
           />
         </InputField>
         {errors.description?.message ? (
-          <StyledErrorMessage>{errors.name?.message}</StyledErrorMessage>
+          <StyledErrorMessage>{errors.description?.message}</StyledErrorMessage>
         ) : null}
+
         {fields.map((field, index) => {
           return (
-            <Classworks key={field.id}>
-              <InputField htmlFor="name">
-                <span>Class Work Name</span>
-                <Input
-                  placeholder="name"
-                  {...register(
-                    `classworks.${index}.name` as const,
-                    classWorkValidationRules.name
-                  )}
-                  defaultValue={field.name}
-                />
-              </InputField>
-              <Input
-                placeholder="description"
-                {...register(
-                  `classworks.${index}.description` as const,
-                  classWorkValidationRules.description
+            <div>
+              <Classworks key={field.id}>
+                <InputField htmlFor="classworkName">
+                  <span>Class Work {index + 1}</span>
+                  <Input
+                    placeholder="name"
+                    {...register(
+                      `classworks.${index}.name` as const,
+                      classWorkValidationRules.name
+                    )}
+                    defaultValue={field.name}
+                  />
+                </InputField>
+                {errors.classworks?.map(({ name }, ind) =>
+                  ind === index ? (
+                    <StyledErrorMessage key={uuidv4()}>
+                      {name?.message}
+                    </StyledErrorMessage>
+                  ) : null
                 )}
-                defaultValue={field.description}
-              />
-
-              <TrashIcon type="button" onClick={() => remove(index)} />
-            </Classworks>
+                <InputField htmlFor="classworkDescription">
+                  <Input
+                    placeholder="description"
+                    {...register(
+                      `classworks.${index}.description` as const,
+                      classWorkValidationRules.description
+                    )}
+                    defaultValue={field.description}
+                  />
+                </InputField>
+                {errors.classworks?.map(({ description }, ind) =>
+                  ind === index ? (
+                    <StyledErrorMessage key={uuidv4()}>
+                      {description?.message}
+                    </StyledErrorMessage>
+                  ) : null
+                )}
+                <TrashIcon type="button" onClick={() => remove(index)} />
+              </Classworks>
+            </div>
           );
         })}
         <PrimaryButton
@@ -167,7 +181,6 @@ export const EditClassModal = ({
           <FaPlus /> New Class Work
         </PrimaryButton>
       </StyledForm>
-      {Object.keys(errors).length > 0 && <FormErrorMessages errors={errors} />}
       {errorMessages?.map(({ msg }) => (
         <StyledErrorMessage key={msg}>{msg}</StyledErrorMessage>
       ))}
